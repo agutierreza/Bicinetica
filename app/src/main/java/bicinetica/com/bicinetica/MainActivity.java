b549c4c1c15220f564c0f910092fa8e64bdfefb5
Francisco José Torralvo <frascoj93@hotmail.com>
2018-02-17 18:16:42 +0100

    First commit

---
+++ app/src/main/java/bicinetica/com/bicinetica/MainActivity.java
@@ -0,0 +1,13 @@
+package bicinetica.com.bicinetica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
