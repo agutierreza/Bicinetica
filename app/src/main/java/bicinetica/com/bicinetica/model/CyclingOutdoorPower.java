f2dc047cd96086f1b1d9bbfd420d563a7b514b25
Francisco José Torralvo <frascoj93@hotmail.com>
2018-03-01 11:01:10 +0100

    Update CyclingOutdoorPower algorithm

--- app/src/main/java/bicinetica/com/bicinetica/model/CyclingOutdoorPower.java
+++ app/src/main/java/bicinetica/com/bicinetica/model/CyclingOutdoorPower.java
@@ -12,36 +12,28 @@ public class CyclingOutdoorPower {
 	private static final float rho = 1.226f;
 	private static final float drag = CdA * rho / 2;
     
-    public static float[] calculatePower(Position pos1, Position pos2) {
-    	
+    public static float calculatePower(Position pos1, Position pos2) {
     	//Calculating increments
-        int secDiff = pos2.getSeconds() - pos1.getSeconds();
 		float hDiff = pos2.getAltitude() - pos1.getAltitude();// It should be minus the Position altitude x meters before (25m-50m is fine). A less demanding approximation is to get altitude from the Position instance x/p1.getAltitude() seconds before.
 
         float grade = hDiff / pos1.getDistance(pos2);
         double beta = Math.atan(grade);
-        float betaSin = (float)Math.sin(beta);
-        float betaCos = (float)Math.cos(beta);
     	
     	//interpolation of velocity for Position p1 and p2
-    	float[] speed = Utilities.linealInterpolation(pos1.getSpeed(), pos2.getSpeed(), secDiff);
 
     	//interpolation of Latitude and Longitude is only needed at the end of the activity, taking consecutive Position instances given by GPS
     	//float[] lat = Utilities.linealInterpolation(pos1.getLatitude(), pos2.getLatitude(), secDiff);
     	//float[] lon = Utilities.linealInterpolation(pos1.getLongitude(), pos2.getLongitude(), secDiff);
 
     	//Calculate power from p1.getSeconds()... p2.getSeconds()-1;
-    	float[] power = new float[secDiff];// secdiff+1 nodes but no power for the last one
 
-    	for (int i = 0; i < secDiff - 1; i++) {
-			float avgSpeed = Utilities.average(speed[i], speed[i + 1]);
-			float pKin = (float)(Math.pow(speed[i+1], 2) - Math.pow(speed[i], 2)) * totalMass / 2;
-			float pGravity = avgSpeed * gForce * totalMass * betaSin;
-			float pDrag = 2 * (float)Math.pow(avgSpeed, 3) * drag;
-			float pRolling = avgSpeed * cRolling * gForce * totalMass * betaCos;
-    		power[i] = pKin + pGravity + pDrag + pRolling;
-    	}
+		float avgSpeed = Utilities.average(pos2.getSpeed(), pos1.getSpeed());
+		float pKin = (float)(Math.pow(pos2.getSpeed(), 2) - Math.pow(pos1.getSpeed(), 2)) * totalMass / 2;
+		float pGravity = avgSpeed * gForce * totalMass * (float)Math.sin(beta);
+		float pDrag = 2 * (float)Math.pow(avgSpeed, 3) * drag;
+		float pRolling = avgSpeed * cRolling * gForce * totalMass * (float)Math.cos(beta);
+		float power = pKin + pGravity + pDrag + pRolling;
 
-    	return power;
+    	return Math.max(0, power);
     }
 }
