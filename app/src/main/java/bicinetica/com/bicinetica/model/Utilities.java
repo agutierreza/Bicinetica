8c7bb243177e8d73bd700a63f8dd264a7eea1f30
agutierreza <36405644+agutierreza@users.noreply.github.com>
2018-03-09 18:02:56 +0100

    Methods cpseg e interpola
    interpola method is best seen in a graph (Paco saw it ant that's what matters for now, but...
    We'll need documentation soon!!!
    
    I tested cpseg a few days ago. Interpola tests are here:
    
    TESTS FOR interpola:
    
    PositionGPS position1 = new PositionGPS(36f, -4f, 215,919.01f);
    PositionGPS position2 = new PositionGPS(37f, -5f,230,920.99f);
    position1.setSpeed((float)(5));
    position2.setSpeed((float) (4));
    
    Leave lat,lon,alt, speed the same. Tests changing seconds
    
    Timestamp primera posicion:919,01
    Timestamp segunda posicion:921,99
    Numero de elementos:4
    elem 0. sec: 919, lat: 35,997 , lon: -3,997, alt: 214,950
    elem 1. sec: 920, lat: 36,332 , lon: -4,332, alt: 219,983
    elem 2. sec: 921, lat: 36,668 , lon: -4,668, alt: 225,017
    elem 3. sec: 922, lat: 37,003 , lon: -5,003, alt: 230,050
    
    
    
    Case: This never happens anyway!! :)
    -->
    Timestamp primera posicion:919,01
    Timestamp segunda posicion:922,00
    Numero de elementos:4
    elem 0. sec: 919, lat: 35,997 , lon: -3,997, alt: 214,950
    elem 1. sec: 920, lat: 36,331 , lon: -4,331, alt: 219,966
    elem 2. sec: 921, lat: 36,666 , lon: -4,666, alt: 224,983
    elem 3. sec: 922, lat: 37,000 , lon: -5,000, alt: 230,000
    
    
    Exact seconds:
    
    Timestamp primera posicion:919,00
    Timestamp segunda posicion:922,00
    Numero de elementos:4
    elem 0. sec: 919, lat: 36,000 , lon: -4,000, alt: 215,000
    elem 1. sec: 920, lat: 36,333 , lon: -4,333, alt: 220,000
    elem 2. sec: 921, lat: 36,667 , lon: -4,667, alt: 225,000
    elem 3. sec: 922, lat: 37,000 , lon: -5,000, alt: 230,000
    
    
    A second between:
    
    Timestamp primera posicion:919,01
    Timestamp segunda posicion:920,99
    Numero de elementos:3
    elem 0. sec: 919, lat: 35,995 , lon: -3,995, alt: 214,924
    elem 1. sec: 920, lat: 36,500 , lon: -4,500, alt: 222,500
    elem 2. sec: 921, lat: 37,005 , lon: -5,005, alt: 230,076
    
    No seconds between!:
    
    Timestamp primera posicion:919,01
    Timestamp segunda posicion:919,99
    Numero de elementos:2
    elem 0. sec: 919, lat: 35,990 , lon: -3,990, alt: 214,847
    elem 1. sec: 920, lat: 37,010 , lon: -5,010, alt: 230,153

--- app/src/main/java/bicinetica/com/bicinetica/model/Utilities.java
+++ app/src/main/java/bicinetica/com/bicinetica/model/Utilities.java
@@ -40,4 +40,76 @@ public final class Utilities {
         for (Position item : items) res += item.getPower();
         return res / items.size();
     }
+    
+    /**
+     * Gives the best average @seg seconds for powerList
+     * 
+     * @param powerList List of activity power, second by second
+     * @param seg The number of seconds to get the best average of
+     */
+    public static int cpseg(ArrayList<Integer> powerList, int seg) {
+    	int suma = 0 , bestSuma = 0;
+    	for (int i=0; i<seg;i++) {
+    		suma+=powerList.get(i);
+    	}
+    	bestSuma=suma;
+    	for (int i=seg; i<powerList.size(); i++) {
+    		suma+=-powerList.get(i-seg)+powerList.get(i);
+    		if (suma > bestSuma) {
+    			bestSuma=suma;
+    		}
+    	}
+    	return Math.round((float)bestSuma/seg);
+    }
+    
+    public static ArrayList<Position> interpola(PositionGPS p1, PositionGPS p2) {
+    	ArrayList<Position> positions = new ArrayList<>();
+    	
+    	//The number of Positions that we are going to add
+    	int numPositions;
+    	if (p2.getSeconds()- (int) p2.getSeconds()==0) {
+    		//This never happens anyway!
+    		numPositions = (int) p2.getSeconds() - (int) p1.getSeconds() + 1;
+    	}else {
+    		numPositions = (int) p2.getSeconds() - (int) p1.getSeconds() + 2;
+    	}
+    	//numPositions= 2+(int) p2.getSeconds() - (int) p1.getSeconds();
+    	
+    	//line parameters for Lat,Lon,Alt, speed
+    	float modLat, modLon, modAlt, modSpd, bLat, bLon, bAlt, bSpd;
+    	float duration = p2.getSeconds()-p1.getSeconds();
+    	
+    	modLat = (p2.getLatitude()  - p1.getLatitude())  / duration;
+    	modLon = (p2.getLongitude() - p1.getLongitude()) / duration;
+    	modAlt = (p2.getAltitude()  - p1.getAltitude())  / duration;
+    	modSpd = (p2.getSpeed()     - p1.getSpeed())     / duration;
+    	
+    	bLat = p1.getLatitude() - modLat*p1.getSeconds();
+    	bLon = p1.getLongitude() - modLon*p1.getSeconds();
+    	bAlt = p1.getAltitude() - modAlt*p1.getSeconds();
+    	bSpd = p1.getSpeed() - modSpd*p1.getSpeed();
+    	
+    	//First point using parameters
+    	int second= (int) p1.getSeconds();
+    	float lat, lon, alt, spd;
+    	lat = modLat*second + bLat;
+    	lon = modLon*second + bLon;
+    	alt = modAlt*second + bAlt;
+    	spd = modSpd*second + bSpd;
+    	Position p = new Position(lat,lon,alt,second,spd);
+    	positions.add(p);
+    	
+    	//Adding the rest from the slope module :)
+    	for (int i = 1; i<numPositions;i++) {
+    		lat+=modLat;
+    		lon+=modLon;
+    		alt+=modAlt;
+    		spd+=modSpd;
+    		second+=1;
+    		positions.add(new Position(lat,lon,alt,second,spd));
+    	}
+    	
+    	return positions;
+    }    
+
 }
