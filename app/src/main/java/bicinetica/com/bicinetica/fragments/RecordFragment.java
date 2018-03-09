a318301453cd5f0d5778913464ee38558cb9f7a9
Francisco José Torralvo <frascoj93@hotmail.com>
2018-03-09 18:20:47 +0100

    Update layout files

--- app/src/main/java/bicinetica/com/bicinetica/fragments/RecordFragment.java
+++ app/src/main/java/bicinetica/com/bicinetica/fragments/RecordFragment.java
@@ -3,6 +3,7 @@ package bicinetica.com.bicinetica.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -60,7 +61,10 @@ public class RecordFragment extends Fragment {
             items.add(r);
 
             recyclerView.setAdapter(new RecordListAdapter(items));
+            recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
         }
+
+
         return view;
     }
 
